97a91a705298ab94756b5f4d1af7bbce8498a7c8
pedropmelojs <pedroyverdon@gmail.com>
2025-11-18 11:24:29 +0100

    displaytr not yet done. got some issues displaying hex address

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rpinheir <rpinheir@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 13:15:40 by rpinheir          #+#    #+#             */
-/*   Updated: 2025/11/14 14:13:47 by rpinheir         ###   ########.fr       */
+/*   Updated: 2025/11/18 11:12:22 by rpinheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,47 +14,53 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-int var_handler(const char *s, int size, va_list  varg)
+int	var_handler(const char *s, int size, va_list varg)
 {
-  if (s[size + 1] == 'c')
-    ft_putchar(va_arg(varg, int));
-  if (s[size + 1] == 's')
-    ft_putstr(va_arg(varg, char *));
-
-  return (size);
+	if (s[size + 1] == 'c')
+		ft_putchar(va_arg(varg, int));
+	if (s[size + 1] == 's')
+		ft_putstr(va_arg(varg, char *));
+	if (s[size + 1] == 'p')
+		ft_displayptr(va_arg(varg, void *), size);
+	return (size);
 }
 
 int	ft_printf(const char *s, ...)
 {
-	int	size;
-  va_list varg;
-  va_start(varg, s);
+	int		size;
+	va_list	varg;
+
+	va_start(varg, s);
 	size = 0;
 	while (s[size] != '\0')
-  {
-    if (s[size] == '%')
-    { 
-      if(s[size + 1] == '%' )
-      {
-        ft_putchar('%');
-        size++;
-      }
-      else
-      {
-        var_handler(s,size,varg);
-        size+=2;
-      }
-    }
-    if (s[size] != '%')
-      ft_putchar(s[size]);
-    size++;
-  }
-  return (size);
+	{
+		if (s[size] == '%')
+		{
+			if (s[size + 1] == '%')
+			{
+				ft_putchar('%');
+				size++;
+			}
+			else
+			{
+				var_handler(s, size, varg);
+				size += 2;
+			}
+		}
+		if (s[size] != '%')
+			ft_putchar(s[size]);
+		size++;
+	}
+	return (size);
 }
 
 int	main(void)
 {
-  char *name = "pedro";
-  char greetings = '~';
-  ft_printf("%c Salut %s !\n",greetings,name );
+	char	*name;
+	char	greetings;
+
+	name = "pedro";
+	greetings = '~';
+	ft_printf("%c Salut %s = %p !\n", greetings, name, &name);
+	printf("%c Salut %s = %p !\n", greetings, name, &name);
 }
